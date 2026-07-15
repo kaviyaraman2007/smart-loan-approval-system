7cb7f4e105a37b1b256074527f8b4a946e4a8593
kaviyaraman2007 <kaviyaraman2007@gmail.com>
2026-07-15 11:36:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -333,10 +333,35 @@ system = SmartLoanSystem()
 # FLASK ROUTES
 # ===========================================
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/index1")
+def index1():
+    return render_template("index1.html")
+
+
+@app.route("/index2")
+def index2():
+    return render_template("index2.html")
+
+
+@app.route("/index3")
+def index3():
+    return render_template("index3.html")
+
+
+@app.route("/index4")
+def index4():
+    return render_template("index4.html")
+
+
+@app.route("/index5")
+def index5():
+    return render_template("index5.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
